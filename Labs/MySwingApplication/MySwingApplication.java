44767c9eee5f785eb92188367f831a95f3a5e390
Jeremiah <6wa9jeremiah@users.noreply.github.com>
2019-02-12 09:46:34 -0800

    GUI update

---
+++ Labs/MySwingApplication/MySwingApplication.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+public class MySwingApplication implements Runnable {
+    // instance variables
+    JFrame jFrame;
+    CanvasComponent canvasComponent;
+    
+    public void run() {
+        // creates and sets up a new window
+        jFrame = new JFrame("Window");
+        jFrame.setSize(400, 400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // new canvas component object
+        canvasComponent = new CanvasComponent(100, 100);
+        jFrame.add(canvasComponent);
+        jFrame.addKeyListener(canvasComponent);
+        jFrame.setVisible(true);
+    }
+    
+    public static void main() {
+        // creates new my swing application object
+        MySwingApplication mySwingApplicationSwing = new MySwingApplication();
+        javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
+    }
+}
