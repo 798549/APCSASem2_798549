0dbc14f48d50bf331609e54c9b6a66bbc7e1c068
Jeremiah <6wa9jeremiah@users.noreply.github.com>
2019-03-17 17:42:08 -0700

    update elevens

--- Projects/Project 6 Elevens/Activity11  Starter Code/ElevensGUIRunner.java
+++ Projects/Project 6 Elevens/Activity11  Starter Code/ElevensGUIRunner.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * This is a class that plays the GUI version of the Elevens game.
  * See accompanying documents for a description of how Elevens is played.
@@ -15,21 +14,3 @@ public class ElevensGUIRunner {
 		gui.displayGame();
 	}
 }
-=======
-/**
- * This is a class that plays the GUI version of the Elevens game.
- * See accompanying documents for a description of how Elevens is played.
- */
-public class ElevensGUIRunner {
-
-	/**
-	 * Plays the GUI version of Elevens.
-	 * @param args is not used.
-	 */
-	public static void main(String[] args) {
-		Board board = new ElevensBoard();
-		CardGameGUI gui = new CardGameGUI(board);
-		gui.displayGame();
-	}
-}
->>>>>>> 04c6c56fed9ce95ed4f2bbfe462e29722529b186
